fbac59e0c77648e3b715216e6ec327b7bf2ed479
packardG <packard.ben.gilson@hpe.com>
2017-09-15 08:36:13 -0400

    Updated hostslists for new cluster

--- cluster_exports.sh
+++ cluster_exports.sh
@@ -14,27 +14,16 @@ function contains() {
 }
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-engvmqa1801=("engvmqa1801" "engvmqa1802" "engvmqa1803" "engvmqa1804")
-engvmqa1805=("engvmqa1805" "engvmqa1806" "engvmqa1807" "engvmqa1808" "engvmqa1809")
+engvmqa1806=("engvmqa1806" "engvmqa1807" "engvmqa1808" "engvmqa1809")
 
-if   [ $(contains "${engvmqa1801[@]}" $(hostname)) == "y" ]; then
-    hostlist=$(join_by , ${engvmqa1801[@]})
+if   [ $(contains "${engvmqa1806[@]}" $(hostname)) == "y" ]; then
+    hostlist=$(join_by , ${engvmqa1806[@]})
     echo "export HOSTLIST=$hostlist"
     echo "export HOST=$hostlist"
-    echo "export NNODES=${#engvmqa1801[@]}"
-    for i in $(seq 1 ${#engvmqa1801[@]})
+    echo "export NNODES=${#engvmqa1806[@]}"
+    for i in $(seq 1 ${#engvmqa1806[@]})
     do
-        echo "export HOST$i=${engvmqa1801[$(expr $i - 1)]}"
-        echo "export NODE$i=node0$i"
-    done
-elif [ $(contains "${engvmqa1805[@]}" $(hostname)) == "y" ]; then
-    hostlist=$(join_by , ${engvmqa1805[@]})
-    echo "export HOSTLIST=$hostlist"
-    echo "export HOST=$hostlist"
-    echo "export NNODES=${#engvmqa1805[@]}"
-    for i in $(seq 1 ${#engvmqa1805[@]})
-    do
-        echo "export HOST$i=${engvmqa1805[$(expr $i - 1)]}"
+        echo "export HOST$i=${engvmqa1806[$(expr $i - 1)]}"
         echo "export NODE$i=node0$i"
     done
 fi
