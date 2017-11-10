0916e656bde33429cebbb2e4020df5bfef68af00
packardG <packard.ben.gilson@hpe.com>
2017-11-10 15:08:26 -0500

    Added new cluster to hostlists & exports

--- cluster_exports.sh
+++ cluster_exports.sh
@@ -15,6 +15,7 @@ function contains() {
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 engvmqa1806=("engvmqa1806" "engvmqa1807" "engvmqa1808" "engvmqa1809")
+qastress55=("qastress-55" "qastress-56" "qastress-57" "qastress-58" "qastress-59" "qastress-60")
 
 if   [ $(contains "${engvmqa1806[@]}" $(hostname)) == "y" ]; then
     hostlist=$(join_by , ${engvmqa1806[@]})
@@ -24,6 +25,18 @@ if   [ $(contains "${engvmqa1806[@]}" $(hostname)) == "y" ]; then
     for i in $(seq 1 ${#engvmqa1806[@]})
     do
         echo "export HOST$i=${engvmqa1806[$(expr $i - 1)]}"
+        echo "export NODE$i=node0$i"
+    done
+fi
+
+if   [ $(contains "${qastress55[@]}" $(hostname)) == "y" ]; then
+    hostlist=$(join_by , ${qastress55[@]})
+    echo "export HOSTLIST=$hostlist"
+    echo "export HOST=$hostlist"
+    echo "export NNODES=${#qastress55[@]}"
+    for i in $(seq 1 ${#qastress55[@]})
+    do
+        echo "export HOST$i=${qastress55[$(expr $i - 1)]}"
         echo "export NODE$i=node0$i"
     done
 fi
